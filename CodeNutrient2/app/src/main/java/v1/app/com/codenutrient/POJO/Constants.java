e4d201a1477af29f66166bb821ca32627179e197
Arturo <arturo9324@gmail.com>
2017-03-11 00:11:47 -0600

    Fix some more bugs

--- CodeNutrient2/app/src/main/java/v1/app/com/codenutrient/POJO/Constants.java
+++ CodeNutrient2/app/src/main/java/v1/app/com/codenutrient/POJO/Constants.java
@@ -1,10 +1,6 @@
 package v1.app.com.codenutrient.POJO;
 
-import com.android.tools.fd.runtime.IncrementalChange;
-import com.android.tools.fd.runtime.InstantReloadException;
-
 public class Constants {
-    public static volatile transient /* synthetic */ IncrementalChange $change = null;
     public static final String app_users = "app_users";
     public static final String calories = "app_user_calories";
     public static final String eaten_nutrients = "eaten_nutrients";
@@ -14,59 +10,4 @@ public class Constants {
     public static final String products = "products/";
     public static final String service_version = "api/v1/";
     public static final String values = "best_nutrient_values";
-
-    Constants(Object[] objArr, InstantReloadException instantReloadException) {
-        switch (((String) objArr[0]).hashCode()) {
-            case -1968665286:
-            case -500045066:
-                this();
-            default:
-                throw new InstantReloadException(String.format("String switch could not find '%s' with hashcode %s in %s", new Object[]{(String) objArr[0], Integer.valueOf(((String) objArr[0]).hashCode()), "v1/app/com/codenutrient/POJO/Constants"}));
-        }
-    }
-
-    public static /* synthetic */ Object access$super(Constants constants, String str, Object... objArr) {
-        switch (str.hashCode()) {
-            case -2128160755:
-                return super.toString();
-            case -1600833221:
-                super.wait(((Number) objArr[0]).longValue(), ((Number) objArr[1]).intValue());
-                return null;
-            case -1554832987:
-                super.finalize();
-                return null;
-            case -1166127280:
-                super.notify();
-                return null;
-            case -1021472056:
-                super.wait(((Number) objArr[0]).longValue());
-                return null;
-            case -712101345:
-                super.notifyAll();
-                return null;
-            case 201261558:
-                return super.getClass();
-            case 244142972:
-                super.wait();
-                return null;
-            case 1403628309:
-                return new Integer(super.hashCode());
-            case 1814730534:
-                return new Boolean(super.equals(objArr[0]));
-            case 2025021518:
-                return super.clone();
-            default:
-                throw new InstantReloadException(String.format("String switch could not find '%s' with hashcode %s in %s", new Object[]{str, Integer.valueOf(str.hashCode()), "v1/app/com/codenutrient/POJO/Constants"}));
-        }
-    }
-
-    public Constants() {
-        IncrementalChange incrementalChange = $change;
-        if (incrementalChange != null) {
-            this((Object[]) incrementalChange.access$dispatch("init$args.([Ljava/lang/Object;)Ljava/lang/Object;", r2), null);
-        }
-        if (incrementalChange != null) {
-            incrementalChange.access$dispatch("init$body.(Lv1/app/com/codenutrient/POJO/Constants;)V", this);
-        }
-    }
 }
