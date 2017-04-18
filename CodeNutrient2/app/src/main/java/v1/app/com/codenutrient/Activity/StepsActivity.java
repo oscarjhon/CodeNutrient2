efc0bfe51819f3b8d61da458900188b6e2fbd5f6
Arturo <arturo9324@gmail.com>
2017-04-17 21:53:55 -0500

    stepschart

--- CodeNutrient2/app/src/main/java/v1/app/com/codenutrient/Activity/StepsActivity.java
+++ CodeNutrient2/app/src/main/java/v1/app/com/codenutrient/Activity/StepsActivity.java
@@ -25,8 +25,11 @@ import java.util.List;
 
 import v1.app.com.codenutrient.HTTP.HttpManager;
 import v1.app.com.codenutrient.Helpers.DataBaseHelper;
+import v1.app.com.codenutrient.POJO.Product;
 import v1.app.com.codenutrient.POJO.StepEntries;
 import v1.app.com.codenutrient.R;
+import v1.app.com.codenutrient.Requests.Calories;
+import v1.app.com.codenutrient.Requests.HasProduct;
 
 /**
  * Created by Arturo on 07/04/2017.
@@ -60,7 +63,8 @@ public class StepsActivity extends AppCompatActivity {
         switch(position){
             case 1:
                 if (manager.isOnLine(getApplicationContext())){
-
+                    GetProducts task = new GetProducts();
+                    task.execute();
                 }else{
                     ShowErrorMessage("Debes tener conexión a internet para consultar esta información");
                 }
@@ -80,24 +84,28 @@ public class StepsActivity extends AppCompatActivity {
 
     public void passStepEntryesToList(ArrayList<StepEntries> entries, String message){
         List<Entry> entryList = new ArrayList<>();
-        for (int i = 1; i <= days; i++){
-            Calendar cal = Calendar.getInstance();
-            cal.set(Calendar.DAY_OF_MONTH, i);
-            boolean isSet = false;
-            for (int j = 0; j < entries.size(); j++){
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTime(entries.get(j).getDay());
-                if (cal.equals(calendar)){
-                    isSet = true;
-                    entryList.add(new Entry(i, entries.get(j).getValue()));
-                    entries.remove(j);
+        if (entries != null) {
+            for (int i = 1; i <= days; i++) {
+                Calendar cal = Calendar.getInstance();
+                cal.set(Calendar.DAY_OF_MONTH, i);
+                boolean isSet = false;
+                for (int j = 0; j < entries.size(); j++) {
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTime(entries.get(j).getDay());
+                    if (cal.get(Calendar.DAY_OF_MONTH) == calendar.get(Calendar.DAY_OF_MONTH)) {
+                        isSet = true;
+                        entryList.add(new Entry(i, entries.get(j).getValue()));
+                        entries.remove(j);
+                    }
+                }
+                if (!isSet) {
+                    entryList.add(new Entry(i, 0));
                 }
             }
-            if (!isSet){
-                entryList.add(new Entry(i, 0));
-            }
+            ShowSuccess(entryList, message);
+        }else{
+            ShowErrorMessage("Ocurrio un error inesperado al obtener tu información");
         }
-        ShowSuccess(entryList, message);
     }
 
     public void ShowErrorMessage(String message){
@@ -198,24 +206,61 @@ public class StepsActivity extends AppCompatActivity {
             for (StepEntries entry : entries){
                 Calendar cal = Calendar.getInstance();
                 cal.setTime(entry.getDay());
-                if (cal.equals(calendar)){
+                if (cal.get(Calendar.DAY_OF_MONTH) == calendar.get(Calendar.DAY_OF_MONTH)){
                     entries1.add(entry);
                 }
             }
         }
-        passStepEntryesToList(entries1, "Pasos recorridos en " + GetMonthName());
+        passStepEntryesToList(entries1, "Pasos recorridos durante " + GetMonthName());
     }
 
     public class GetProducts extends  AsyncTask<String, String, ArrayList<StepEntries>> {
 
         @Override
         protected ArrayList<StepEntries> doInBackground(String... params) {
-            return new ArrayList<>();
+            boolean reloaded = false;
+            Calendar cal = Calendar.getInstance();
+            Calendar today = Calendar.getInstance();
+            HasProduct request = new HasProduct();
+            ArrayList<StepEntries> entities = new ArrayList<>();
+            for (int i = 1; i <= days; i++){
+                v1.app.com.codenutrient.POJO.HasProduct products;
+                if(cal.get(Calendar.DAY_OF_MONTH) <= today.get(Calendar.DAY_OF_MONTH)) {
+                    if (cal.get(Calendar.DAY_OF_MONTH) == today.get(Calendar.DAY_OF_MONTH)){
+                        products = request.ExecuteGET(MainActivity.appUser);
+                    }else {
+                        cal.set(Calendar.DAY_OF_MONTH, i);
+                        products =request.ExecuteGET(MainActivity.appUser, cal.getTime());
+                    }
+                    if (products.getCode() == 200){
+                        int calories = 0;
+                        for(Product p : products.getProducts()){
+                            calories += p.getCalorias();
+                        }
+                        StepEntries entries = new StepEntries();
+                        entries.setValue(calories);
+                        entries.setDay(cal.getTime());
+                        entities.add(entries);
+                    }else if (products.getCode() == 401){
+                        if (!reloaded) {
+                            if (manager.reload(1, getApplicationContext())){
+                                reloaded = true;
+                                i--;
+                            }
+                        }
+                    }else{
+                        return null;
+                    }
+
+                }
+            }
+            return entities;
         }
 
         @Override
         protected void onPostExecute(ArrayList<StepEntries> entries) {
             super.onPostExecute(entries);
+            passStepEntryesToList(entries, "Calorías consumidas durante " + GetMonthName() );
         }
     }
 
@@ -223,7 +268,47 @@ public class StepsActivity extends AppCompatActivity {
 
         @Override
         protected ArrayList<StepEntries> doInBackground(String... params) {
-            return new ArrayList<>();
+            boolean reloaded = false;
+            Calendar cal = Calendar.getInstance();
+            Calendar today = Calendar.getInstance();
+            Calories request = new Calories();
+            ArrayList<StepEntries> entities = new ArrayList<>();
+            for (int i = 1; i <= days; i++){
+                v1.app.com.codenutrient.POJO.Calories calories;
+                if(cal.get(Calendar.DAY_OF_MONTH) <= today.get(Calendar.DAY_OF_MONTH)) {
+                    if (cal.get(Calendar.DAY_OF_MONTH) == today.get(Calendar.DAY_OF_MONTH)){
+                         calories = request.ExecuteGET(MainActivity.appUser);
+                    }else {
+                        cal.set(Calendar.DAY_OF_MONTH, i);
+                        calories =request.ExecuteGET(MainActivity.appUser, cal.getTime());
+                    }
+                    if (calories.getCode() == 200){
+                        StepEntries entries = new StepEntries();
+                        entries.setValue(calories.getGasto());
+                        entries.setDay(cal.getTime());
+                        entities.add(entries);
+                    }else if (calories.getCode() == 401){
+                        if (!reloaded) {
+                            if (manager.reload(1, getApplicationContext())){
+                                reloaded = true;
+                                i--;
+                            }else{
+                                return null;
+                            }
+                        }
+                    }else{
+                        return null;
+                    }
+
+                }
+            }
+            return entities;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<StepEntries> entries) {
+            super.onPostExecute(entries);
+            passStepEntryesToList(entries, "Calorías quemadas durante " + GetMonthName() );
         }
     }
 
